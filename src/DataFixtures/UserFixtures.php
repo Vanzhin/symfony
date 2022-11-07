caf7b1f9ffe0e24afd7bd61df4f56a3dd10a0177
nikolay vanzhin <nikolay.vanzhin@yandex.ru>
2022-11-07 16:53:06 +0500

    8.4 auth created

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,16 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends BaseFixtures
 {
+
+
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function loadData(ObjectManager $manager): void
     {
         $this->createMany(User::class, 10, function (User $user) use ($manager) {
             $user
                 ->setName($this->faker->firstName())
                 ->setEmail($this->faker->email())
-                ->setPassword($this->faker->password());
+                ->setPassword($this->hasher->hashPassword($user, '123456789'));
         });
     }
 }
